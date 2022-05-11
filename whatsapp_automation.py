beae0880f618bd0c50a8b51c63e1beef142e6811
AVINASH <71930072+Avinash-kapuganti@users.noreply.github.com>
2022-05-11 09:41:35 +0530

    Update whatsapp_automation.py

--- whatsapp_automation.py
+++ whatsapp_automation.py
@@ -1,30 +1,4 @@
-"""The parameters for pywhatkit to send whatsapp message
 import pywhatkit as kit
-kit.sendwhatmsg("+91(phone_number)",message ,time_hour ,time_min)
-
-phone_num (required) - Phone number of target with country code
-
-message (required) - Message that you want to sendwhatmsg
-
-time_hour (required) - Hours at which you want to send message in 24 hour format
-
-time_min (required) - Minutes at which you want to send message
-
-Some common errors
-
-CountryCodeException - Check if the phone number passed into the parameter has country code
-
-Message not getting delivered - Check internet speed and increase wait_time to 30 or above
-
-CallTimeException - The web takes some time to load so some delay is required,
-make sure the seconds left is greater than the wait_time
-
-SyntaxError - Make sure the first two parameters are string and the rest are int"""
-
-import pywhatkit as kit
-
-
-
 import speech_recognition as sr
 import pyttsx3
 
@@ -40,22 +14,17 @@ def SpeakText(command):
 	engine.say(command)
 	engine.runAndWait()
 	
-	
-
 try:
-		
+	
 	
 	with sr.Microphone() as source2:
                                                
-			
-	   
 	    print("speak")
 	    r.adjust_for_ambient_noise(source2, duration=0.2)
 			
 	   
 	    audio2 = r.listen(source2)
 			
-	   
 	    MyText = r.recognize_google(audio2)
 	    MyText = MyText.lower()
 
